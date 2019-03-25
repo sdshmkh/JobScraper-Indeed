4fa88a202b7f0d9ae484ed4d6ff9e002da200606
Shahaji <shahaji31.deshmukh@gmail.com>
2019-03-24 20:07:54 -0400

    All data compiled into single array of objects

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+var chalk = require('chalk');
 var scrapeSummary = require('./scrapeSummary');
+var scrapePosting = require('./scrapePosting');
+var config = require('./config.json');
 
 const main = async function() {
-    let data = await scrapeSummary(11790, 0);
-    console.log(data);
+    let dataSet = [];
+    let postingDataSet = [];
+    let title = config.jobtitle.split(" ").join("+");
+    for (let index = 0; index < config.results; index++) {
+        let data = await scrapeSummary(config.zipCode, index*10, title);
+        dataSet.push(data);
+    }
+    dataSet.forEach(data => {
+        data.forEach(async (jobSummary) => {
+            let posting = await scrapePosting(jobSummary.link);
+            posting = Object.assign(posting, {Title : jobSummary.title, CompanyName : jobSummary.companyName, Salary : jobSummary.salary, Location : jobSummary.location})
+            postingDataSet.push(posting);
+        });
+    });
+    return postingDataSet;
 };
 
 main();
